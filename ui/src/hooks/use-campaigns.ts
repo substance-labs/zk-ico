cbd35cf0c63e1725a5b9f22da2dbd72510b968dd
envin <envin@proton.me>
2025-06-27 18:00:15 +0200

    feat: add api call

--- ui/src/hooks/use-campaigns.ts
+++ ui/src/hooks/use-campaigns.ts
@@ -6,8 +6,11 @@ import BigNumber from "bignumber.js"
 
 import { useAppStore } from "../store.js"
 import zkIcoAbi from "../utils/abi/zkico.json"
+import zkIcoContractBytecode from "../utils/bytecodes/zkico.json"
+import zkIcoTokenBytecode from "../utils/bytecodes/token.json"
 import type { Campaign, CreateCampaign } from "../types.js"
 import { getZkPassportProof } from "../utils/zkpassport.js"
+import { useDeployContract } from "./use-deploy.js"
 
 const TOPIC = "0x531026765026b9af1528359f0fb0ffd560e49666995880799502227196c5d897"
 
@@ -22,7 +25,7 @@ export const useCampaigns = () => {
         `https://api.etherscan.io/v2/api?chainid=${baseSepolia.id}&module=logs&action=getLogs&topic0=${TOPIC}&apikey=${process.env.ETHERSCAN_API_KEY}`,
       )
       const zkIcoAddresses = result.map(({ address }: any) => address).slice(3) // FIXME remove first one as it's wrong.
-
+      console.log("zkIcoAddresses", zkIcoAddresses)
       const client = createPublicClient({
         chain: baseSepolia,
         transport: http(),
@@ -128,9 +131,45 @@ export const useParticipateToCampaign = () => {
 }
 
 export const useCreateCampaign = () => {
+  const { deployContract, isDeploying, deploymentResult, resetDeployment } = useDeployContract()
   const create = useCallback(async (params: CreateCampaign) => {
     try {
-      // TODO integrate here
+      // deploy the the ICO token
+      const {name: tokenName, symbol: tokenSymbol} = params.icoToken
+      if (!tokenName || !tokenSymbol) {
+        throw new Error("ICO token name and symbol are required")
+      }
+      
+      const tokenAmount = '1000000000000000000'
+      const mintToAddress = process.env.VITE_MINT_TO_ADDRESS
+      
+      if (!mintToAddress) {
+        throw new Error("Mint to address not configured. Please set VITE_MINT_TO_ADDRESS in your environment variables.")
+      }
+      
+      const tokenDeploymentResult = await deployContract({
+        bytecode: zkIcoTokenBytecode.object,
+        constructorArgs: [tokenName, tokenSymbol, mintToAddress, tokenAmount],
+        networkId: baseSepolia.id,
+      })
+      resetDeployment()
+      const zkIcoDeploymentResult = await deployContract({
+        bytecode: zkIcoContractBytecode.object,
+        constructorArgs: [
+          params.gateway,
+          params.aztecBuyTokenAddress,
+          params.buyTokenAddress,
+          tokenDeploymentResult.contractAddress,
+          params.verifier,
+          params.rate,
+          params.title,
+          params.description
+        ],
+        networkId: baseSepolia.id,
+      })
+      return [tokenDeploymentResult, zkIcoDeploymentResult]
+      
+      // deploy the zkICO contract with the provided parameters
     } catch (err) {
       console.error(err)
     }
