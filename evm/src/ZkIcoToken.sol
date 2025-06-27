b07bb8c56b9bb45742a32f8b13ecd8857077b02e
Alessandro Manfredi <alle.manfredi@gmail.com>
2025-06-27 13:36:50 +0200

    chore(evm): rn Token into ZkIcoToken

--- evm/src/ZkIcoToken.sol
+++ evm/src/ZkIcoToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.28;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
+contract ZkIcoToken is ERC20 {
     constructor(string memory name, string memory symbol, address to, uint256 amount) ERC20(name, symbol) {
         _mint(to, amount);
     }
